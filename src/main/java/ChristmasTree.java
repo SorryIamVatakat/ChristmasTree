8303ebccd561606ec8385d20aa98753f52c9429c
SorryIamVatakat <SorryIamVatakat@users.noreply.github.com>
2020-02-28 13:39:33 +0200

    add Leg

--- src/main/java/ChristmasTree.java
+++ src/main/java/ChristmasTree.java
@@ -3,26 +3,30 @@ public class ChristmasTree {
     private static final int MIDDLE = 10;
 
 
-
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int a = 0;
-        while(a < MIDDLE){
+        while (a < MIDDLE) {
             int i = 0;
-            while(i < MIDDLE - a) {
+            while (i < MIDDLE - a) {
                 System.out.print(" ");
                 i++;
             }
             int j = 0;
-            while(j < a*2 + 1) {
+            while (j < a * 2 + 1) {
                 System.out.print("*");
                 j++;
             }
             System.out.println();
             a++;
         }
+        for (int i2 = 0; i2 < 4; i2++) {
+            for (int j2 = 0; j2 < 6; j2++) {
+                System.out.print("*");
+            }
+            System.out.println();
 
 
+        }
 
     }
 }
